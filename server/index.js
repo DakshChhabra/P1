1946e713dd9669612f718fdd382b0d8683448a04
DakshChhabra <dakshchhabra2108@gmail.com>
2025-12-27 12:41:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -740,6 +740,12 @@ app.get('/*', (req, res) => {
 
 } */
 
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
 
     app.listen(PORT, () => {
         console.log(`Server running on http://localhost:${PORT}`);
