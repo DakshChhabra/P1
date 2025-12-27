c0b6cc28297790187f807d393c162eb53aca68bf
DakshChhabra <dakshchhabra2108@gmail.com>
2025-12-27 12:28:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -733,9 +733,10 @@ app.post("/gradePvMove", async (req, res) => {
 else{
     app.use(express.static(path.join(__dirname, '../build')));
     
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
+
 
 }
 
